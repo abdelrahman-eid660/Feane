43448f35d0c342f59d12c9f81171080218e64d77
Abdelrahman Eid <eida0556@gmail.com>
2025-08-18 23:06:25 +0300

    update

--- js/main.js
+++ js/main.js
@@ -3,35 +3,19 @@ let body = document.body
 let allbtn = document.querySelectorAll("#menu button") 
 let h2menu = document.querySelectorAll("body h2") 
 let icon = document.querySelector('.icon')
-console.log(icon);
-
 // ================= Dark Mood ===========================
-let mood = 'dark'
-function changeMood(){
-    if(mood === "dark"){
-        icon.classList.replace('fa-moon','fa-sun');
-        body.style.backgroundColor = "#2c2c2c "
-        for(let i = 0 ; i <allbtn.length; i++ ){
-            allbtn[i].style.color = "white"
-            allbtn[i].style.backgroundColor = "black"
-        }
-        for (let J = 0; J < h2menu.length; J++) {
-            h2menu[J].style.color = "white";
-        }
-        mood = 'light';
-    }
-    else{
-        icon.classList.replace('fa-sun','fa-moon');
-        body.style.backgroundColor = ""
-        for (let i = 0; i < allbtn.length; i++) {
-            allbtn[i].style.backgroundColor = "";
-            allbtn[i].style.color = "";
-        }
-        for (let J = 0; J < h2menu.length; J++) {
-            h2menu[J].style.color = "";
-        }
-        mood = 'dark';
-    }
+if(localStorage.getItem("theme") === "dark"){
+    body.classList.add("dark-mode")
+    icon.classList.replace("fa-moon", "fa-sun");
 }
-darkmood.addEventListener("click",changeMood)
-
+darkmood.addEventListener("click",()=>{
+    body.classList.toggle("dark-mode")
+    if(body.classList.contains("dark-mode")){
+        icon.classList.replace("fa-moon", "fa-sun");
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        icon.classList.replace("fa-sun", "fa-moon");
+        localStorage.setItem("theme", "light");
+    }
+})
